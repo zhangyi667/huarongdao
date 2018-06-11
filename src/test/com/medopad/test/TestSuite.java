23b196362b03e3e70e1ba6b005eea9c45eef2fd0
Your Name <you@example.com>
2018-06-11 21:50:11 +0100

    Add test.

--- src/test/com/medopad/test/TestSuite.java
+++ src/test/com/medopad/test/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestPuzzleParser.class, TestMaps.class, TestPoints.class
+        TestPuzzleParser.class, TestMaps.class, TestPoints.class, TestSnapshotFactory.class
 })
 public class TestSuite {
 }
